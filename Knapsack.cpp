a9b1354377c89e3ee7c3579323bd5a4189feea8f
hodgsontet <hodgsontet@gmail.com>
2021-08-11 16:31:07 -0400

    problem formulation

---
+++ Knapsack.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <string>
+#include <bits/stdc++.h>
+using namespace std;  
+
+struct Node 
+{ int weightAvail ;
+  int ItemNum;
+
+}
+
+Node arr[24];
+int num_of_items = 3;
+int weightLim = 5;
+
+int value[3] = {10,4,7};
+int weights[3] = {4,2,3};
+
+int Node_index = 0;
+for (int i = 0; i <=weightLim ; ++i)
+{
+	for (int j = 0; j < num_of_items; ++i)
+	{
+		/* code */
+	}
+}
